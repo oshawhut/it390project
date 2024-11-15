fa08ca0a6108e889dee2bfba5984edd734db6161
Dina <avalonstar05@gmail.com>
2024-11-15 12:36:16 -0500

    Manually merged and edited Austin's code to main branch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/footer.css";
+
+const Footer = () => {
+	return (
+		<header className="footer">
+			<p>
+				@ 2024. Copyright by Madina Ea, Alysa Nuyen, Long Tran, Mariia Ahmud,
+				and Austin White{" "}
+			</p>
+		</header>
+	);
+};
+
+export default Footer;
